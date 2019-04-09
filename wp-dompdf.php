db52e931869e633d81c05ac53ea5c8940b9600e2
Daniel Henríquez Sandoval <dhenriquez@cor-dhenriquez-mac.local>
2019-04-09 17:24:21 -0400

    dompdf

--- wp-dompdf.php
+++ wp-dompdf.php
@@ -5,6 +5,18 @@
     Plugin URI: https://www.dhenriquez.cl
     Author: Daniel Henríquez
     Author URI: https://www.dhenriquez.cl
-    Version: 0.0.0
+    Version: 0.8.3
 */
+
+define('WP_DOMPDF_PLUGIN', plugin_dir_path(__FILE__));
+
+require_once WP_DOMPDF_PLUGIN . 'lib/dompdf/autoload.inc.php';
+use Dompdf\Dompdf;
+
+if(class_exists('Dompdf')){
+    function wp_dompdf(){
+        return new Dompdf();
+    }
+}
+
 ?>
