5063ac641eef329dbb015f235a1b40b2934bd2e2
Daniel Henríquez Sandoval <dhenriquez@cor-dhenriquez-mac.local>
2019-04-09 17:16:34 -0400

    Lib: DOMPDF

---
+++ wp-dompdf.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    Plugin Name: WP DomPDF
+    Description: Plugin para utilizar librería DomPDF en Wordpress
+    Plugin URI: https://www.dhenriquez.cl
+    Author: Daniel Henríquez
+    Author URI: https://www.dhenriquez.cl
+    Version: 0.0.0
+*/
+?>
